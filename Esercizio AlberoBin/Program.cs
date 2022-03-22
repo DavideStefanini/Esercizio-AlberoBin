c3ae5b04156b9d9d6468ef1eb9b989a1d2c75e55
DavideStefanini <davide.stefanini05@gmail.com>
2022-03-22 17:26:35 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio AlberoBin/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_AlberoBin
+{
+    class Program
+    {
+        class Nodo
+        {
+            int val;
+            Nodo succDx;
+            Nodo succSx;
+
+            public Nodo(int val)
+            {
+                this.val = val;
+            }
+
+            public void setSuccDx(Nodo n) 
+            {
+                this.succDx = n;
+            }
+
+            public void setSuccSx(Nodo n) 
+            {
+                this.succSx = n;
+            }
+
+            public int getVal()
+            {
+                return val;
+            }
+
+            public void setVal(int val)
+            {
+                this.val = val;
+            }
+        }
+
+        class AlberoBinario
+        {
+            Nodo radice;
+            AlberoBinario dx;
+            AlberoBinario sx;
+
+            public AlberoBinario(Nodo n)
+            {
+                radice = n;
+            }
+            public AlberoBinario() { }
+
+            public void insDX(int val)
+            {
+                Nodo n = new Nodo(val);
+                if (radice == null)
+                {
+                    radice = n;
+                }
+                else
+                {
+                    n.setSuccDx(radice);
+                    radice = n;
+                }
+            }
+            public void insSX(int val)
+            {
+                Nodo n = new Nodo(val);
+                if (radice == null)
+                {
+                    radice = n;
+                }
+                else
+                {
+                    n.setSuccSx(radice);
+                    radice = n;
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            AlberoBinario a = new AlberoBinario();
+            a.insDX(69);
+            a.insDX(23);
+            a.insSX(33);
+        }
+
+    }
+}
